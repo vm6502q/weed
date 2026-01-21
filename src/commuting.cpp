9d09c7ad5178e74c6b22cb9687ce5c2bb55717f6
WrathfulSpatula <stranoj@gmail.com>
2026-01-20 21:36:03 -0500

    Handle mixed

--- src/commuting.cpp
+++ src/commuting.cpp
@@ -52,7 +52,7 @@
   size_t n = b->size;                                                          \
   pfControl.par_for(0, n, fn)
 
-#define DISPATCH_GPU_KERNEL(type, api_add, api_mul)                            \
+#define DISPATCH_GPU_KERNEL(type, type2, api_add, api_mul)                     \
   OCLAPI api_call;                                                             \
   switch (op) {                                                                \
   case CommutingOperation::MUL:                                                \
@@ -65,20 +65,8 @@
   const vecCapIntGpu args[2U]{a.offset, b.offset};                             \
   std::shared_ptr<type> a_storage =                                            \
       std::dynamic_pointer_cast<type>(a.storage);                              \
-  std::shared_ptr<type> b_storage =                                            \
-      std::dynamic_pointer_cast<type>(b.storage);                              \
-  std::shared_ptr<type> o_storage =                                            \
-      std::dynamic_pointer_cast<type>(out.storage);                            \
-  a_storage->gpu->RequestKernel(                                               \
-      api_call, args, a.get_size(),                                            \
-      {a_storage->buffer, b_storage->buffer, o_storage->buffer})
-
-#define DISPATCH_MIXED_GPU_KERNEL(api_call)                                    \
-  const vecCapIntGpu args[2U]{a.offset, b.offset};                             \
-  std::shared_ptr<type> a_storage =                                            \
-      std::dynamic_pointer_cast<type>(a.storage);                              \
-  std::shared_ptr<type> b_storage =                                            \
-      std::dynamic_pointer_cast<type>(b.storage);                              \
+  std::shared_ptr<type2> b_storage =                                           \
+      std::dynamic_pointer_cast<type2>(b.storage);                             \
   std::shared_ptr<type> o_storage =                                            \
       std::dynamic_pointer_cast<type>(out.storage);                            \
   a_storage->gpu->RequestKernel(                                               \
@@ -111,14 +99,18 @@ struct commuting_kernel : CommutingKernel {
     KERNEL_SWITCH();
   }
   void gpu_real(const Tensor &a, const Tensor &b, Tensor &out) {
-    DISPATCH_GPU_KERNEL(GpuRealStorage, OCLAPI::OCL_API_ADD_REAL,
-                        OCLAPI::OCL_API_MUL_REAL);
+    DISPATCH_GPU_KERNEL(GpuRealStorage, GpuRealStorage,
+                        OCLAPI::OCL_API_ADD_REAL, OCLAPI::OCL_API_MUL_REAL);
   }
   void gpu_complex(const Tensor &a, const Tensor &b, Tensor &out) {
-    DISPATCH_GPU_KERNEL(GpuComplexStorage, OCLAPI::OCL_API_ADD_COMPLEX,
+    DISPATCH_GPU_KERNEL(GpuComplexStorage, GpuComplexStorage,
+                        OCLAPI::OCL_API_ADD_COMPLEX,
                         OCLAPI::OCL_API_MUL_COMPLEX);
   }
-  void gpu_mixed(const Tensor &a, const Tensor &b, Tensor &out) {}
+  void gpu_mixed(const Tensor &a, const Tensor &b, Tensor &out) {
+    DISPATCH_GPU_KERNEL(GpuComplexStorage, GpuRealStorage,
+                        OCLAPI::OCL_API_ADD_MIXED, OCLAPI::OCL_API_MUL_MIXED);
+  }
 
   void cpu_real_inplace(StoragePtr a, const StoragePtr b) {
     CAST_STORAGE(pa, a, real1, CpuRealStorage);
