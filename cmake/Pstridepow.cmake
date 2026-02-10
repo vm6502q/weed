edf229a26754c6c27d4a38c03c32fad738ca6221
WrathfulSpatula <stranoj@gmail.com>
2026-02-09 23:23:43 -0500

    Raise default PSTRIDEPOW

--- cmake/Pstridepow.cmake
+++ cmake/Pstridepow.cmake
@@ -1,2 +1,2 @@
-math(EXPR ADJ_PSTRIDEPOW "20 - ${WEED_FPPOW}")
+math(EXPR ADJ_PSTRIDEPOW "23 - ${WEED_FPPOW}")
 set(WEED_PSTRIDEPOW "${ADJ_PSTRIDEPOW}" CACHE STRING "Stride of parallel for loops (as a power of 2)")
