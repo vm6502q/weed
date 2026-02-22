286c79060edb2aa63557e65f48f5feecf4ffa11d
WrathfulSpatula <stranoj@gmail.com>
2026-02-22 11:27:13 -0500

    Debug transformer example

--- examples/binary_addition_transformer.cpp
+++ examples/binary_addition_transformer.cpp
@@ -10,14 +10,15 @@
 // https://www.gnu.org/licenses/lgpl-3.0.en.html for details.
 
 #include "autograd/adam.hpp"
-#include "autograd/bci_loss.hpp"
+#include "autograd/bci_with_logits_loss.hpp"
 #include "autograd/zero_grad.hpp"
 #include "modules/embedding.hpp"
 #include "modules/linear.hpp"
 #include "modules/positional_encoding.hpp"
 #include "modules/sequential.hpp"
-#include "modules/sigmoid.hpp"
+#include "modules/tanh.hpp"
 #include "modules/transformer_encoder_layer.hpp"
+#include "tensors/real_scalar.hpp"
 #include "tensors/symbol_tensor.hpp"
 
 #define GET_REAL(ptr) static_cast<RealScalar *>((ptr).get())->get_item()
@@ -76,48 +77,49 @@ BinaryAdditionSample generate_samples(int bit_width, int samples) {
 
 int main() {
   const tcapint bit_width = 4;
-  const tcapint seq_len = bit_width * 2 + 2; // a + b =
-  const tcapint target_len = bit_width + 1;  // sum bits
+  const tcapint seq_len = (bit_width << 1U) + 2U; // a + b =
+  const tcapint target_len = bit_width + 1U;  // sum bits
   const tcapint vocab_size = 5;
 
-  const tcapint d_model = 32;
-  const tcapint d_ff = 64;
-  const tcapint num_heads = 4;
+  const tcapint d_model = 16;
+  const tcapint d_ff = 32;
+  const tcapint num_heads = 2;
 
   const int epochs = 2000;
   const int batch_size = 32;
 
   // ---- Model ----
   Sequential model({
-    std::make_shared<Embedding>(vocab_size, d_model),
-    std::make_shared<PositionalEncoding>(seq_len, d_model),
-    std::make_shared<TransformerEncoderLayer>(d_model, num_heads, d_ff),
-    std::make_shared<Linear>(d_model, 1),
-    std::make_shared<Sigmoid>()}
-  );
+      std::make_shared<Embedding>(vocab_size, d_model),
+      std::make_shared<PositionalEncoding>(seq_len, d_model),
+      std::make_shared<TransformerEncoderLayer>(d_model, num_heads, d_ff),
+      std::make_shared<Linear>(d_model, 2),
+      std::make_shared<Tanh>(),
+      std::make_shared<Linear>(2, 1)
+  });
 
   Adam optimizer(R(0.001));
   optimizer.register_parameters(model.parameters());
 
+  auto sample = generate_samples(bit_width, batch_size);
+
+  auto input = std::make_shared<SymbolTensor>(
+      sample.input_tokens, std::vector<tcapint>{batch_size, seq_len},
+      std::vector<tcapint>{1, batch_size});
+
+  auto target = std::make_shared<Tensor>(
+      sample.target_bits, std::vector<tcapint>{batch_size, target_len},
+      std::vector<tcapint>{1, batch_size});
+
   // ---- Training ----
   for (int epoch = 0; epoch < epochs; ++epoch) {
-    auto sample = generate_samples(bit_width, batch_size);
-
-    auto input = std::make_shared<SymbolTensor>(
-        sample.input_tokens, std::vector<tcapint>{batch_size, seq_len},
-        std::vector<tcapint>{1, batch_size});
-
     auto logits = model.forward(input);
 
     // We take only last (target_len) positions
     auto predicted =
-        Tensor::slice(logits, 1, seq_len - target_len, target_len)->squeeze(2);
+        Tensor::slice(logits, 1, logits->shape[1U] - target_len, target_len)->squeeze(2);
 
-    auto target = std::make_shared<Tensor>(
-        sample.target_bits, std::vector<tcapint>{batch_size, target_len},
-        std::vector<tcapint>{1, batch_size});
-
-    auto loss = bci_loss(predicted, target);
+    auto loss = bci_with_logits_loss(predicted, target);
 
     Tensor::backward(loss);
 
