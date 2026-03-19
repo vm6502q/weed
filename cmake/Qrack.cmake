e88b5dd11691e0af3c882efa40b1b75727012a50
WrathfulSpatula <stranoj@gmail.com>
2026-03-19 17:08:57 -0400

    WEED_ENABLE_QRACK

--- cmake/Qrack.cmake
+++ cmake/Qrack.cmake
@@ -1,3 +1,5 @@
+option(WEED_ENABLE_QRACK "Enable Qrack, if available" ON)
+
 if (NOT QRACK_DIR)
 set(QRACK_DIR "/usr/local/lib/qrack")
 set(QRACK_INCLUDE "/usr/local/include")
@@ -7,7 +9,7 @@ find_library(QRACK_LIB
     NAMES qrack libqrack
     PATHS "${QRACK_DIR}")
 
-if (QRACK_LIB)
+if (QRACK_LIB AND WEED_ENABLE_QRACK)
     message(STATUS "Found Qrack library: ${QRACK_LIB}")
     target_link_directories(weed PUBLIC ${QRACK_DIR})
     target_include_directories(weed PUBLIC ${QRACK_INCLUDE})
@@ -17,4 +19,4 @@ if (QRACK_LIB)
         src/modules/qrack_neuron.cpp
         src/modules/qrack_neuron_layer.cpp
         )
-endif (QRACK_LIB)
+endif (QRACK_LIB AND WEED_ENABLE_QRACK)
